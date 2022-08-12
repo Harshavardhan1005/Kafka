cbdc8abd7658ab88e0afcf0d2c43df6f5d255c26
Harsha <harshavardhan.achyuta@gmail.com>
2022-08-12 14:50:16 +0200

    Done

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
-## Streaming Open Weather API Data and storing in Postgresql and HDFS 
+# Streaming Open Weather API Data and storing in Postgresql and HDFS 
 
 # Prerequisites
 
@@ -20,24 +20,25 @@ Spark and Kafka Installation
 Dependency Jar are downloaded from below links
 
 !!! Note
-https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10_2.12/3.0.1
-https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients/2.6.0
-https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10-assembly_2.12/3.0.1
-https://mvnrepository.com/artifact/org.postgresql/postgresql/42.2.16
-https://mvnrepository.com/artifact/org.apache.commons/commons-pool2/2.8.1
+
+> https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10_2.12/3.0.1
+> https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients/2.6.0
+> https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10-assembly_2.12/3.0.1
+> https://mvnrepository.com/artifact/org.postgresql/postgresql/42.2.16
+> https://mvnrepository.com/artifact/org.apache.commons/commons-pool2/2.8.1
 
 
-# Docker file to run zookeeper and kafka 
+## Docker file to run zookeeper and kafka 
 ```bash
 docker-compose up -d
 ```
 
-# Run to publish the open weather data to kafka topic
+## Run to publish the open weather data to kafka topic
 ```bash
 python weather_live_data.py
 ```
 
-# Run to subscribe the data and store it using pyspark in Postgresql and HDFS
+## Run to subscribe the data and store it using pyspark in Postgresql and HDFS
 ```bash
 python process_data_spark.py
 ```
---
+++ docs/src/weather_live_data.py
@@ -0,0 +1,65 @@
+from kafka import KafkaProducer
+from datetime import datetime
+import time
+from json import dumps
+import requests
+
+TOPIC_NAME_CONS = "topic-weather"
+BOOTSTRAP_SERVERS_CONS = 'localhost:9092'
+
+def get_data(url):
+
+    response_data = requests.get(url=url)
+    data = response_data.json()
+    weather = {}
+    event_datetime = datetime.now()
+    weather["event_datetime"] 		= event_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    weather["humidity"] 			= data["main"]["humidity"]
+    weather["pressure"] 			= data["main"]["pressure"]
+    weather["temperature"] 			= data["main"]["temp"]
+    weather["temperature_max"] 		= data["main"]["temp_max"]
+    weather["temperature_min"] 		= data["main"]["temp_min"]
+    weather["temperature_feels"] 	= data["main"]["feels_like"]
+    weather["city_name"] 			= data["name"]
+    weather["country_name"] 		= data["sys"]["country"]
+    weather["time_zone"] 			= data["timezone"]
+    weather["visibility"] 			= data["visibility"]
+    weather["wind_deg"] 			= data["wind"]["deg"]
+    weather["wind_speed"] 			= data["wind"]["speed"]
+
+    return weather
+
+
+if __name__ == "__main__":
+    print("Open Weather API Data | Kafka Producer Application Started ... ")
+
+    kafka_producer_obj = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS_CONS,
+                             value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+    city_names = ["Hamburg","Berlin","Frankfurt am Main","Stuttgart","Munich","Düsseldorf","Köln","Dresden","Leipzig"]
+    
+    i = 0
+    while True:
+        try:
+
+            for city in city_names:
+                api_key = "< Open Weather API KEY >"
+                url = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}".format(city,api_key)
+                weather_data = get_data(url)
+                i = i + 1
+                print("<<<<<<<<<<  Live Straeming Data >>>>>>>>>>")
+                print("Printing message id: " + str(i))
+                print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                print("Sending message to Kafka topic: " + TOPIC_NAME_CONS)
+                print("Message to be sent: ", weather_data)
+                kafka_producer_obj.send(TOPIC_NAME_CONS, weather_data)
+
+        except Exception as ex:
+            print("Event Message Construction Failed. ")
+            print(ex)
+
+        time.sleep(10)
+
+        
+
+
